282d0f2a644eace1edec156feec079c322ca99cc
Muditha <kabmmadusith2003@gmail.com>
2023-05-27 13:07:28 +0530

    Update Grid
    Created a fully responsive grid for the Hot Bids section.

---
+++ components/Grid/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styles from './index.module.css'
+import customStyles from './customStyles.module.css'
+import { Container, Row, Col } from 'react-grid-system';
+import { NFTcardDesktop } from '../Cards/NFTcardDesktop';
+import { NFTcardMobile } from '../Cards/NFTcardMobile';
+
+export const Grid = () => {
+  return (
+      <div className={styles.div}>
+          <Container className={styles.grid_container} fluid>
+              <Row debug className={styles.row} justify='center' style={{backgroundColor: 'transparent', margin:'0' }} >
+                  <Col debug xs={6} sm={5} md={5} lg={3} className={styles.col} style={{outline: 'none', backgroundColor: 'transparent'}}>
+                      <NFTcardDesktop/>
+                      <NFTcardMobile/>
+                  </Col>
+                  <Col debug xs={6} sm={5} md={5} lg={3} className={styles.col} style={{outline: 'none', backgroundColor: 'transparent'}} >
+                      <NFTcardDesktop/>
+                      <NFTcardMobile/>
+                  </Col>
+                  <Col debug xs={6} sm={5} md={5} lg={3} className={styles.col} style={{outline: 'none', backgroundColor: 'transparent'}} >
+                      <NFTcardDesktop/>
+                      <NFTcardMobile/>
+                  </Col>
+                  <Col debug xs={6} sm={5} md={5} lg={3} className={styles.col} style={{outline: 'none', backgroundColor: 'transparent'}} >
+                       <NFTcardDesktop/>
+                      <NFTcardMobile/>
+                  </Col>
+              </Row>
+              <Row debug className={styles.row} justify='center' style={{backgroundColor: 'transparent',  margin:'0'}}>
+                  <Col debug xs={6} sm={5} md={5} lg={3} className={styles.col} style={{outline: 'none', backgroundColor: 'transparent'}} >
+                      <NFTcardDesktop/>
+                      <NFTcardMobile/>
+                  </Col>
+                  <Col debug xs={6} sm={5} md={5} lg={3} className={styles.col} style={{outline: 'none', backgroundColor: 'transparent'}} >
+                      <NFTcardDesktop/>
+                      <NFTcardMobile/>
+                  </Col>
+                  <Col debug xs={6} sm={5} md={5} lg={3} className={styles.col} style={{outline: 'none', backgroundColor: 'transparent'}} >
+                      <NFTcardDesktop/>
+                      <NFTcardMobile/>
+                  </Col>
+                  <Col debug xs={6} sm={5} md={5} lg={3} className={styles.col} style={{outline: 'none', backgroundColor: 'transparent'}} >
+                      <NFTcardDesktop/>
+                      <NFTcardMobile/>
+                  </Col>
+              </Row>
+          </Container>
+      </div>
+  )
+}
--- components/Home/Section3/index.tsx
+++ components/Home/Section3/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from './index.module.css'
+import { Grid } from '@/components/Grid'
 
 export const Section3 = () => {
   return (
     <>
       <div className={styles.back}>
         <div className={styles.content}>
-          <h3 className={styles.h3}>Hot Bids</h3>
-          
+          <div className={styles.h3_div}>
+            <h3 className={styles.h3}>Hot Bids</h3>
+          </div>
+          <Grid/>
         </div>
       </div>
     </>
