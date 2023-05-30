cd0f605a893ee134c202ff2e48cc8c226d599fd2
Muditha <kabmmadusith2003@gmail.com>
2023-05-30 14:34:40 +0530

    Section1 updated

--- components/Home/Section1/index.tsx
+++ components/Home/Section1/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './index.module.css'
+import Banner from '../../../public/Banner.png'
+import Image from 'next/image'
 
 const Section1 = () => {
   return (
     <div className={styles.black_back}>
       <div className={styles.pink_box}>
-        <h1 className={styles.h1}>Discover, collect, and sell <br /> extraordinary NFTs</h1>
+        <Image src={Banner} alt='banner' className={styles.bg}></Image>
+        <div className={styles.content}>
+          <h1 className={styles.h1}>Discover, collect, and sell <br /> extraordinary NFTs</h1>
+        </div>
       </div>
     </div>
   )
