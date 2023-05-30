dbfa3102679a1716e9ce15ada853343339cd8214
Muditha <kabmmadusith2003@gmail.com>
2023-05-30 13:14:30 +0530

    Set routing for NFT-item and profile.

--- components/Cards/NFTcardDesktop/index.tsx
+++ components/Cards/NFTcardDesktop/index.tsx
@@ -2,35 +2,38 @@ import React from 'react'
 import styles from './index.module.css'
 import NFT from '../../../public/images/NFT.png'
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import Link from 'next/link';
 import Image from 'next/image';
 
 const NFTcardDesktop = () => {
   return (
-    <div className={styles.back}>
-      <div className={styles.card}>
-        <div className={styles.im}>
-          <Image
-            className={styles.img}
-            src={NFT}
-            alt='NFT'>
-          </Image>
-        </div>
-        <div className={styles.nft_name}>
-          <p className={styles.p}>Abstact Smoke Red Blue</p>
-        </div>
-        <div className={styles.price_likes}>
-          <div className={styles.price}>
+    <Link href="/nft-item" style={{ textDecoration: 'inherit', color: 'white' }}>
+      <div className={styles.back}>
+        <div className={styles.card}>
+          <div className={styles.im}>
+            <Image
+              className={styles.img}
+              src={NFT}
+              alt='NFT'>
+            </Image>
+          </div>
+          <div className={styles.nft_name}>
+            <p className={styles.p}>Abstact Smoke Red Blue</p>
+          </div>
+          <div className={styles.price_likes}>
+            <div className={styles.price}>
               <p className={styles.p1}><b>1.25</b></p>
               <p className={styles.p1}>ETH</p>
-          </div>
-          <div className={styles.likes}>
-              <FavoriteIcon 
-              className={styles.icon}/>
+            </div>
+            <div className={styles.likes}>
+              <FavoriteIcon
+                className={styles.icon} />
               <p className={styles.p1}>92</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 export default NFTcardDesktop;
--- components/Cards/SellercardMobile/index.tsx
+++ components/Cards/SellercardMobile/index.tsx
@@ -2,37 +2,40 @@ import React from 'react';
 import styles from './index.module.css'
 import Profile from '../../../public/images/profile.png'
 import CheckIcon from '@mui/icons-material/Check';
+import Link from 'next/link';
 import Image from 'next/image'
 
 const SellercardMobile = () => {
   return (
-    <div className={styles.back}>
-      <div className={styles.card}>
-        <div className={styles.numbdiv}>
-          <div className={styles.number}>1</div>
-        </div>
-        <div className={styles.propic}>
-          <Image
-            src={Profile}
-            alt='profile'
-            className={styles.img}>
-          </Image>
-          <div className={styles.checkdiv}>
-            <div className={styles.check}>
-              <CheckIcon
-              style={{fontSize: 'small'}}/>
+    <Link href="/profile" style={{ textDecoration: 'inherit', color: 'white' }}>
+      <div className={styles.back}>
+        <div className={styles.card}>
+          <div className={styles.numbdiv}>
+            <div className={styles.number}>1</div>
+          </div>
+          <div className={styles.propic}>
+            <Image
+              src={Profile}
+              alt='profile'
+              className={styles.img}>
+            </Image>
+            <div className={styles.checkdiv}>
+              <div className={styles.check}>
+                <CheckIcon
+                  style={{ fontSize: 'small' }} />
+              </div>
+            </div>
+          </div>
+          <div className={styles.name_revenue}>
+            <p className={styles.name}>Mia Ayana</p>
+            <div className={styles.revenue}>
+              <p className={styles.p1}><b>0</b></p>
+              <p className={styles.p1}>ETH</p>
             </div>
           </div>
         </div>
-        <div className={styles.name_revenue}>
-          <p className={styles.name}>Mia Ayana</p>
-          <div className={styles.revenue}>
-            <p className={styles.p1}><b>0</b></p>
-            <p className={styles.p1}>ETH</p>
-          </div>
-        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 export default SellercardMobile;
