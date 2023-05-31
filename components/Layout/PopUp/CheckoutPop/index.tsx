740c9e97c53f05f0468266e016a987f66393a907
Muditha <kabmmadusith2003@gmail.com>
2023-05-31 11:41:47 +0530

    Update popups.

--- components/Layout/PopUp/CheckoutPop/index.tsx
+++ components/Layout/PopUp/CheckoutPop/index.tsx
@@ -4,64 +4,70 @@ import NFT from '../../../../public/images/NFT.png';
 import { useState } from 'react';
 
 import Image from 'next/image';
+import PaysuccesPop from '../PaysuccesPop';
 
 const CheckoutPop = ({setIsOpen, isOpen}: any) => {
 
-//   const [paysMsg, setPaysMsg] = useState(false);
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
 
-//   const doPayment = () => {
-//     setIsOpen(false)
-//     setPaysMsg(true)
-//   }
 
-  const handleClick = () => {
+  const handleClick: any = () => {
+    setPaymentSuccess(true);
+  }
+
+  const handleCancel: any = () => {
     setIsOpen(!isOpen)
   }
 
+
   return (
-<>
-    <div className={styles.pop}>
-        <div className={styles.box}>
-            <div className={styles.head}>
-                <h2 className={styles.h2}>Check Out</h2>
-            </div>
-            <div className={styles.details}>
-                <div className={styles.h}>
-                    <p className={styles.p1}>Item</p>
-                    <p className={styles.p1}>Subtotal</p>
-                </div>
-                <div className={styles.b}>
-                    <div className={styles.img_name}>
-                        <Image
-                        src={NFT}
-                        alt='NFT'
-                        className={styles.img}>
-                        </Image>
-                        <div className={styles.b1}>
-                            <p className={styles.p1}>Mia Ayana</p>
-                            <p className={styles.p2}>Abstact Smoke Red Blue</p>
-                        </div>
-                    </div>
-                    <p className={styles.p1}>4.5 <b>ETH</b></p>
-                </div>
-                <div className={styles.f}>
-                    <p className={styles.p1}>Total</p>
-                    <p className={styles.p1}>4.5 <b>ETH</b></p>
-                </div>
-            </div>
-            <div className={styles.buttons}>
-                <div>
-                    <button className={styles.co_btn}
-                    >Checkout</button>
-                </div>
-                <div>
-                    <button className={styles.ca_btn}
-                    onClick={handleClick}>Cancel</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    </>
+      <>
+          {paymentSuccess ? (
+              <PaysuccesPop setPaymentSuccess={setPaymentSuccess} paymentSuccess={paymentSuccess} isOpen={isOpen} setIsOpen={setIsOpen}/>
+          ) : (
+              <div className={styles.pop} >
+                  <div className={styles.box}>
+                      <div className={styles.head}>
+                          <h2 className={styles.h2}>Check Out</h2>
+                      </div>
+                      <div className={styles.details}>
+                          <div className={styles.h}>
+                              <p className={styles.p1}>Item</p>
+                              <p className={styles.p1}>Subtotal</p>
+                          </div>
+                          <div className={styles.b}>
+                              <div className={styles.img_name}>
+                                  <Image
+                                      src={NFT}
+                                      alt='NFT'
+                                      className={styles.img}>
+                                  </Image>
+                                  <div className={styles.b1}>
+                                      <p className={styles.p1}>Mia Ayana</p>
+                                      <p className={styles.p2}>Abstact Smoke Red Blue</p>
+                                  </div>
+                              </div>
+                              <p className={styles.p1}>4.5 <b>ETH</b></p>
+                          </div>
+                          <div className={styles.f}>
+                              <p className={styles.p1}>Total</p>
+                              <p className={styles.p1}>4.5 <b>ETH</b></p>
+                          </div>
+                      </div>
+                      <div className={styles.buttons}>
+                          <div>
+                              <button className={styles.co_btn}
+                                  onClick={handleClick}>Checkout</button>
+                          </div>
+                          <div>
+                              <button className={styles.ca_btn}
+                                  onClick={handleCancel}>Cancel</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          )}
+      </>
   )
 }
 
--- components/Layout/PopUp/PaysuccesPop/index.tsx
+++ components/Layout/PopUp/PaysuccesPop/index.tsx
@@ -4,13 +4,31 @@ import NFT from '../../../../public/images/NFT.png';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import TelegramIcon from '@mui/icons-material/Telegram';
+import { useEffect, useRef } from 'react';
 
 import Image from 'next/image';
 
-const PaysuccesPop = () => {
+const PaysuccesPop = ({paymentSuccess, setPaymentSuccess, isOpen, setIsOpen}: any) => {
+
+    const popRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      const handleClickOutside: any = (event: MouseEvent) => {
+        if (popRef.current && !popRef.current.contains(event.target as Node)) {
+          setPaymentSuccess(false);
+          setIsOpen(false);
+        }
+      };
+  
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [setPaymentSuccess, setIsOpen]);
+
   return (
     <div className={styles.pop}>
-        <div className={styles.box}>
+        <div className={styles.box}  ref={popRef}>
             <div className={styles.head}>
                 <h2 className={styles.h2}>Payment Successful</h2>
             </div>
